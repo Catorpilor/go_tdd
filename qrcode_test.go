0d6f9d58fe14d930b2de7069b9e28f8e654d53d6
jiaojiangong <jiaojg@meican.com>
2015-07-13 17:24:56 +0800

    Red/Green/Refactor Cycle A full procedure of TDD

--- qrcode_test.go
+++ qrcode_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"image/png"
 	"testing"
 )
 
 func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 	buf := new(bytes.Buffer)
-	GenerateQRCode(buf, "555-2368")
+	GenerateQRCode(buf, "555-2368", Version(1))
 	if buf.Len() == 0 {
 		t.Errorf("Generated QRCode has no data")
 	}
@@ -16,5 +17,43 @@ func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 	_, err := png.Decode(buf)
 	if err != nil {
 		t.Errorf("Generated QRCode not a png: %s", err)
+	}
+}
+
+type ErrorWriter struct{}
+
+func (e *ErrorWriter) Write(b []byte) (int, error) {
+	return 0, errors.New("Expected error")
+}
+
+func TestGenerateQRCodePropagatesErrors(t *testing.T) {
+	w := new(ErrorWriter)
+	err := GenerateQRCode(w, "555-2368", Version(1))
+	if err == nil || err.Error() != "Expected error" {
+		t.Errorf("Expected error ,but got %v", err)
+	}
+}
+
+func TestVersionDeterminesSize(t *testing.T) {
+	table := []struct {
+		version  int
+		expected int
+	}{
+		{1, 21},
+		{2, 25},
+		{6, 41},
+		{7, 45},
+		{14, 73},
+		{40, 177},
+	}
+
+	for _, test := range table {
+		buf := new(bytes.Buffer)
+		GenerateQRCode(buf, "555-2368", Version(test.version))
+		img, _ := png.Decode(buf)
+
+		if width := img.Bounds().Dx(); width != test.expected {
+			t.Errorf("Version %2d, expected %3d but got %3d", test.version, test.expected, width)
+		}
 	}
 }
--- qrcoded.go
+++ qrcoded.go
@@ -4,20 +4,34 @@ import (
 	"image"
 	"image/png"
 	"io"
+	"log"
 	"os"
 )
 
-func GenerateQRCode(w io.Writer, code string) {
-	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+type Version int8
+
+func (v Version) MappedSize() int {
+	return 4*int(v) + 17
+}
+
+func GenerateQRCode(w io.Writer, code string, version Version) error {
+	size := version.MappedSize()
+	img := image.NewNRGBA(image.Rect(0, 0, size, size))
 	//buf := new(bytes.Buffer)
-	_ = png.Encode(w, img)
+	return png.Encode(w, img)
 	//return buf.Bytes()
 }
 
 func main() {
-	file, _ := os.Create("qrcode.png")
+	file, err := os.Create("qrcode.png")
 	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	GenerateQRCode(file, "555-2368")
+	err = GenerateQRCode(file, "555-2368", Version(1))
+	if err != nil {
+		log.Fatal(err)
+	}
 	//ioutil.WriteFile("qrcode.png", qrcode, 0644)
 }
